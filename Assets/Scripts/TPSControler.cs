f342b5d93681f3b6a2ec9eaf746e2a8f57f026ef
RonixKuinn <154350005+RonixKuinn@users.noreply.github.com>
2024-11-11 12:22:34 +0100

    juas

---
+++ Assets/Scripts/TPSControler.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class TPSControler : MonoBehaviour
+{
+    //---------Componentes-----------//
+    private CharacterController _controller;
+    private Transform _camera;
+    private Transform _lookAtPlayer;
+
+    //---------Inputs-----------//
+    private float _horizontal;
+    private float _vertical;
+    
+    [SerializeField] private float _movementSpeed = 5;
+
+    [SerializeField] private float _jumpHeight = 1f;
+
+    [SerializeField] private float _pushForce = 10;
+
+    //---------Gavedad-----------//
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private Vector3 _playerGravity;
+
+    //---------Ground sensor-----------//
+    [SerializeField] Transform _sensorPosition;
+    [SerializeField] float _sensorRadius = 0.5f;
+    [SerializeField] LayerMask _sueloLayer;
+
+    [SerializeField] private AxisState xAsis;
+    [SerializeField] private AxisState yAsis;
+
+     void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _camera = Camera.main.transform;
+        _lookAtPlayer = GameObject.Find("LookAtPlayer").transform;
+    }
+
+    void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
+
+        Gravity();
+        Movement();
+    }
+
+    void Movement()
+    {
+        Vector3 direction = new Vector3(_horizontal, 0, _vertical);
+        
+        yAsis.Update(Time.deltaTime);
+        xAsis.Update(Time.deltaTime);
+
+        transform.rotation = Quaternion.Euler(0, xAsis.Value, 0);
+        _lookAtPlayer.rotation = Quaternion.Euler(xAsis.Value, yAsis.Value, 0);
+    }
+
+    void Gravity()
+    {
+        if(!IsGrounded())
+        {
+            _playerGravity.y += _gravity * Time.deltaTime;
+        }
+        else if (IsGrounded() && _playerGravity.y < 0)
+        {
+            _playerGravity.y = -1;
+        }
+       
+        _controller.Move(_playerGravity * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        _playerGravity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _sueloLayer);
+    }
+
+}
